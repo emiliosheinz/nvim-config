38ecd97df351e7787fbe2f358049b00a0d6b202e
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-07-01 21:26:07 -0300

    feat: add auto tag

--- lua/emiliosheinz/plugins/treesitter.lua
+++ lua/emiliosheinz/plugins/treesitter.lua
@@ -1,45 +1,43 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  dependencies = { "windwp/nvim-ts-autotag" },
-  config = function()
-    local treesitter = require("nvim-treesitter.configs")
-
-    treesitter.setup({
-      highlight = {
-        enable = true,
-      },
-      indent = { enable = true },
-      autotag = {
-        enable = true,
-      },
-      ensure_installed = {
-        "json",
-        "javascript",
-        "typescript",
-        "tsx",
-        "yaml",
-        "html",
-        "css",
-        "prisma",
-        "markdown",
-        "markdown_inline",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
+	dependencies = { "windwp/nvim-ts-autotag" },
+	config = function()
+		local treesitter = require("nvim-treesitter.configs")
+		treesitter.setup({
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+			ensure_installed = {
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"lua",
+				"yaml",
+				"html",
+				"css",
+				"prisma",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"vim",
+				"dockerfile",
+				"gitignore",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
