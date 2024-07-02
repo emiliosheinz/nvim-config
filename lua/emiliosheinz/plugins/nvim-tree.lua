be8982e0a96ebbba7abc947aa5df3837edd681d0
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-07-01 21:57:08 -0300

    feat: reduce explorer width

--- lua/emiliosheinz/plugins/nvim-tree.lua
+++ lua/emiliosheinz/plugins/nvim-tree.lua
@@ -17,7 +17,7 @@ return {
 					open_win_config = function()
 						local screen_w = vim.opt.columns:get()
 						local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-						local window_w = screen_w * 0.8
+						local window_w = screen_w * 0.5
 						local window_h = screen_h * 0.8
 						local window_w_int = math.floor(window_w)
 						local window_h_int = math.floor(window_h)
