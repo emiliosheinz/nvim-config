c57e2a5adb6648d9f00d0b10dca4b28748be2bad
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-07-01 21:04:18 -0300

    feat: improve telescope and colorscheme code

--- lua/emiliosheinz/plugins/colorscheme.lua
+++ lua/emiliosheinz/plugins/colorscheme.lua
@@ -1,14 +1,11 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha"
-      })
-
-      vim.cmd([[colorscheme catppuccin]])
-    end,
-  }
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({ flavour = "mocha" })
+			vim.cmd([[colorscheme catppuccin]])
+		end,
+	},
 }
