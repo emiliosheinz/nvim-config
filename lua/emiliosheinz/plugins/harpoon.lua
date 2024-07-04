7fcc28fc949a042891d65f5d733d45fd882f92c9
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-07-04 18:56:23 -0300

    feat: improve harpoon description

--- lua/emiliosheinz/plugins/harpoon.lua
+++ lua/emiliosheinz/plugins/harpoon.lua
@@ -1,4 +1,4 @@
-return { 
+return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
@@ -9,16 +9,32 @@ return {
 		harpoon:setup()
 		-- REQUIRED
 
-		vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
-		vim.keymap.set("n", "<leader>hk", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+		vim.keymap.set("n", "<leader>ha", function()
+			harpoon:list():add()
+		end, { desc = "Add file to Harpoon menu" })
+		vim.keymap.set("n", "<leader>hk", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "Open Harpoon menu" })
 
-		vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
-		vim.keymap.set("n", "<leader>h2", function() harpoon:list():select(2) end)
-		vim.keymap.set("n", "<leader>h3", function() harpoon:list():select(3) end)
-		vim.keymap.set("n", "<leader>h4", function() harpoon:list():select(4) end)
+		vim.keymap.set("n", "<leader>h1", function()
+			harpoon:list():select(1)
+		end, { desc = "Harpoon the 1st file" })
+		vim.keymap.set("n", "<leader>h2", function()
+			harpoon:list():select(2)
+		end, { desc = "Harpoon the 2nd file" })
+		vim.keymap.set("n", "<leader>h3", function()
+			harpoon:list():select(3)
+		end, { desc = "Harpoon the 3rd file" })
+		vim.keymap.set("n", "<leader>h4", function()
+			harpoon:list():select(3)
+		end, { desc = "Harpoon the 5ft file" })
 
 		-- Toggle previous & next buffers stored within Harpoon list
-		vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
-		vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
+		vim.keymap.set("n", "<leader>hp", function()
+			harpoon:list():prev()
+		end, { desc = "Harpoon previous file" })
+		vim.keymap.set("n", "<leader>hn", function()
+			harpoon:list():next()
+		end, { desc = "Harpoon next file" })
 	end,
 }
