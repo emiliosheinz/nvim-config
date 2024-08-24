609317d96797894657e1f0b23cc27bc31075b204
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-08-24 18:56:48 -0300

    feat: add syntax highlighting for plantuml

--- lua/emiliosheinz/plugins/treesitter.lua
+++ lua/emiliosheinz/plugins/treesitter.lua
@@ -1,27 +1,30 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
-	dependencies = { "windwp/nvim-ts-autotag" },
-	config = function()
-		local treesitter = require("nvim-treesitter.configs")
-		treesitter.setup({
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
+  dependencies = {
+    "windwp/nvim-ts-autotag",
+    "aklt/plantuml-syntax", -- Syntax highlighting for plantuml
+  },
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+    treesitter.setup({
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+  end,
 }
