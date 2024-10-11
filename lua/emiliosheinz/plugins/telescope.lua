e0251de4542e9eaa1350735ec6c8051924823512
Emilio Heinzmann <emilio.heinzmann@cloudbeds.com>
2024-10-11 14:50:48 -0300

    feat: improve telescope quickfix

--- lua/emiliosheinz/plugins/telescope.lua
+++ lua/emiliosheinz/plugins/telescope.lua
@@ -23,6 +23,7 @@ return {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
             ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+            ["<C-s>"] = actions.send_selected_to_qflist + actions.open_qflist,
           },
         },
       },
