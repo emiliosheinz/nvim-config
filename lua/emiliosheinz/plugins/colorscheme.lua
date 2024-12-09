b35e8e35d45f5e26800b7f5677ead4327b590e70
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-12-09 19:48:48 -0300

    feat: improve colorscheme

--- lua/emiliosheinz/plugins/colorscheme.lua
+++ lua/emiliosheinz/plugins/colorscheme.lua
@@ -1,11 +1,14 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({ flavour = "mocha" })
-			vim.cmd([[colorscheme catppuccin]])
-		end,
-	},
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        integrations = { mason = true },
+      })
+      vim.cmd([[colorscheme catppuccin]])
+    end,
+  },
 }
