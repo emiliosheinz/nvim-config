78e8ce5ed14448b82eda1a2eec6a52fcd6867e3d
Emilio Heinzmann <emilio.heinzmann@cloudbeds.com>
2024-12-10 16:52:04 -0300

    feat: improve copilot

--- lua/emiliosheinz/plugins/github-copilot.lua
+++ lua/emiliosheinz/plugins/github-copilot.lua
@@ -1,66 +1,72 @@
 return {
-	{
-		"github/copilot.vim",
-		init = function()
-			vim.g.copilot_no_tab_map = true
-		end,
-		config = function()
-			vim.keymap.set("i", "<C-a>", 'copilot#Accept("\\<CR>")', {
-				expr = true,
-				replace_keycodes = false,
-			})
-			vim.keymap.set("i", "<C-w>", "<Plug>(copilot-accept-word)")
-			vim.keymap.set("i", "<C-e>", "<Plug>(copilot-accept-line)")
-			vim.keymap.set("i", "<C-d>", "<Plug>(copilot-dismiss)")
-			-- Disable copilot by default
-			vim.cmd(":Copilot disable")
-		end,
-	},
-	{
-		"CopilotC-Nvim/CopilotChat.nvim",
-		branch = "canary",
-		dependencies = {
-			{ "github/copilot.vim" },
-			{ "nvim-lua/plenary.nvim" },
-		},
-		build = "make tiktoken", -- Only on MacOS or Linux
-		config = function()
-			local chat = require("CopilotChat")
-			local select = require("CopilotChat.select")
-			local chat_actions = require("CopilotChat.actions")
-			local chat_telescope = require("CopilotChat.integrations.telescope")
+  {
+    "github/copilot.vim",
+    init = function()
+      vim.g.copilot_no_tab_map = true
+    end,
+    config = function()
+      vim.keymap.set("i", "<C-a>", 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+      vim.keymap.set("i", "<C-w>", "<Plug>(copilot-accept-word)")
+      vim.keymap.set("i", "<C-e>", "<Plug>(copilot-accept-line)")
+      vim.keymap.set("i", "<C-d>", "<Plug>(copilot-dismiss)")
+      -- Disable copilot by default
+      vim.cmd(":Copilot disable")
+    end,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "github/copilot.vim" },
+      { "nvim-lua/plenary.nvim" },
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    config = function()
+      local chat = require("CopilotChat")
+      local select = require("CopilotChat.select")
+      local chat_actions = require("CopilotChat.actions")
+      local chat_telescope = require("CopilotChat.integrations.telescope")
 
-			require("CopilotChat.integrations.cmp").setup()
+      require("CopilotChat.integrations.cmp").setup()
 
-			chat.setup({
-				window = {
-					layout = "float",
-					width = 0.8,
-					height = 0.8,
-					row = 2,
-				},
-				selection = function(source)
-					return select.visual(source) or select.buffer(source)
-				end,
-			})
+      chat.setup({
+        window = {
+          layout = "float",
+          width = 0.8,
+          height = 0.8,
+          row = 2,
+        },
+        selection = function(source)
+          return select.visual(source) or select.buffer(source)
+        end,
+        prompts = {
+          ExtractTranslationsSource = {
+            prompt =
+            'Please, give me a valid JSON where the key and value are the parameters being passed to the several lang function calls. E.g.: lang("parameter") should result in {"parameter": "parameter"}.',
+          },
+        },
+      })
 
-			vim.keymap.set(
-				{ "n", "v" },
-				"<leader>cco",
-				":CopilotChatOpen<CR>",
-				{ desc = "CopilotChat - Open chat window" }
-			)
+      vim.keymap.set(
+        { "n", "v" },
+        "<leader>cco",
+        ":CopilotChatOpen<CR>",
+        { desc = "CopilotChat - Open chat window" }
+      )
 
-			vim.keymap.set({ "n", "v" }, "<leader>ccq", function()
-				local input = vim.fn.input("Quick Chat: ")
-				if input ~= "" then
-					chat.ask(input)
-				end
-			end, { desc = "CopilotChat - Quick chat" })
+      vim.keymap.set({ "n", "v" }, "<leader>ccq", function()
+        local input = vim.fn.input("Quick Chat: ")
+        if input ~= "" then
+          chat.ask(input)
+        end
+      end, { desc = "CopilotChat - Quick chat" })
 
-			vim.keymap.set({ "n", "v" }, "<leader>ccp", function()
-				chat_telescope.pick(chat_actions.prompt_actions())
-			end, { desc = "CopilotChat - Prompt action" })
-		end,
-	},
+      vim.keymap.set({ "n", "v" }, "<leader>ccp", function()
+        chat_telescope.pick(chat_actions.prompt_actions())
+      end, { desc = "CopilotChat - Prompt action" })
+    end,
+  },
 }
