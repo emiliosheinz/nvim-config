f23f47b9b07f9b681678ac419d25b06819bf3571
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-12-14 12:50:33 -0300

    refactor: disable format on save

--- lua/emiliosheinz/plugins/none-ls.lua
+++ lua/emiliosheinz/plugins/none-ls.lua
@@ -1,31 +1,31 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = { "nvimtools/none-ls-extras.nvim" },
-	requires = { "nvim-lua/plenary.nvim" },
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  dependencies = { "nvimtools/none-ls-extras.nvim" },
+  requires = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			root_dir = require("null-ls.utils").root_pattern(".git", "package.json"),
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.biome.with({ only_local = "node_modules/.bin" }),
-				null_ls.builtins.formatting.prettierd.with({ only_local = "node_modules/.bin" }),
-				require("none-ls.diagnostics.eslint_d").with({ only_local = "node_modules/.bin" }),
-				require("none-ls.formatting.eslint_d").with({ only_local = "node_modules/.bin" }),
-			},
-			-- Configures autoformatting on save
-			on_attach = function(_, bufnr)
-				vim.api.nvim_create_autocmd("BufWritePre", {
-					group = augroup,
-					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.format()
-					end,
-				})
-			end,
-		})
+    null_ls.setup({
+      root_dir = require("null-ls.utils").root_pattern(".git", "package.json"),
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.biome.with({ only_local = "node_modules/.bin" }),
+        null_ls.builtins.formatting.prettier.with({ only_local = "node_modules/.bin" }),
+        require("none-ls.diagnostics.eslint").with({ only_local = "node_modules/.bin" }),
+        require("none-ls.formatting.eslint").with({ only_local = "node_modules/.bin" }),
+      },
+      -- Configures autoformatting on save
+      -- on_attach = function(_, bufnr)
+      --   vim.api.nvim_create_autocmd("BufWritePre", {
+      --     group = augroup,
+      --     buffer = bufnr,
+      --     callback = function()
+      --       vim.lsp.buf.format()
+      --     end,
+      --   })
+      -- end,
+    })
 
-		vim.keymap.set({ "n", "v" }, "<leader>gf", vim.lsp.buf.format, { desc = "Format file or selection" })
-	end,
+    vim.keymap.set({ "n", "v" }, "<leader>gf", vim.lsp.buf.format, { desc = "Format file or selection" })
+  end,
 }
