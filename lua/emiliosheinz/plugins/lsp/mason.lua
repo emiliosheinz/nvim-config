ae9f9fa9f6a4451520841637f116f55afb4b1b7a
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-12-17 20:18:58 -0300

    refactor: add biome to ensure installed and prefer local

--- lua/emiliosheinz/plugins/lsp/mason.lua
+++ lua/emiliosheinz/plugins/lsp/mason.lua
@@ -1,43 +1,44 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			-- available LSP servers: https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"html",
-					"cssls",
-					"lua_ls",
-					"ts_ls",
-					"tailwindcss",
-				},
-			})
-		end,
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		config = function()
-			require("mason-tool-installer").setup({
-				ensure_installed = {
-					"stylua",
-					"eslint_d",
-					"prettierd",
-				},
-			})
-		end,
-	},
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      -- available LSP servers: https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "html",
+          "cssls",
+          "lua_ls",
+          "ts_ls",
+          "tailwindcss",
+        },
+      })
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = {
+          "stylua",
+          "eslint",
+          "prettier",
+          "biome",
+        },
+      })
+    end,
+  },
 }
--- lua/emiliosheinz/plugins/none-ls.lua
+++ lua/emiliosheinz/plugins/none-ls.lua
@@ -22,7 +22,7 @@ local is_prettier_supported = function()
 		if not root_dir then
 			return false
 		end
-    local package_json_path = root_dir .. "/" .. "package.json"
+		local package_json_path = root_dir .. "/" .. "package.json"
 		local package_json = vim.fn.json_decode(vim.fn.readfile(package_json_path))
 		return package_json and package_json.eslintConfig
 	end
@@ -55,7 +55,7 @@ local is_eslint_supported = function()
 		if not root_dir then
 			return false
 		end
-    local package_json_path = root_dir .. "/" .. "package.json"
+		local package_json_path = root_dir .. "/" .. "package.json"
 		local package_json = vim.fn.json_decode(vim.fn.readfile(package_json_path))
 		return package_json and package_json.eslintConfig
 	end
@@ -74,56 +74,56 @@ local is_biome_supported = function()
 end
 
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = { "nvimtools/none-ls-extras.nvim" },
-  requires = { "nvim-lua/plenary.nvim" },
-  config = function()
-	local null_ls = require("null-ls")
-	null_ls.setup({
-		sources = {
-			null_ls.builtins.formatting.stylua,
-			null_ls.builtins.formatting.biome.with({
-				only_local = "node_modules/.bin",
-				condition = is_biome_supported,
-			}),
-			null_ls.builtins.formatting.prettier.with({
-				only_local = "node_modules/.bin",
-				condition = is_prettier_supported,
-			}),
-			require("none-ls.diagnostics.eslint").with({
-				only_local = "node_modules/.bin",
-			  condition = is_eslint_supported,
-			}),
-			require("none-ls.formatting.eslint").with({
-				only_local = "node_modules/.bin",
-				condition = is_eslint_supported,
-			}),
-		},
-		-- Configures autoformatting on save
-		-- on_attach = function(_, bufnr)
-		-- 	vim.api.nvim_create_autocmd("BufWritePre", {
-		-- 		group = augroup,
-		-- 		buffer = bufnr,
-		-- 		callback = function()
-		-- 			vim.lsp.buf.format()
-		-- 		end,
-		-- 	})
-		-- end,
-	})
-
-    vim.keymap.set({ "n", "v" }, "ff", function()
-		local last_line = vim.fn.line("$")
-		local last_column = vim.fn.col({ last_line, "$" }) - 1
-		vim.lsp.buf.format({
-			range = {
-				-- Format from first to the last line of the the buffer for better performance
-				-- On visual mode it is automatically set to the visual selection
-				-- For some reason lines are 1 indexed and columns 0 🧐
-				-- See: https://neovim.io/doc/user/lsp.html#vim.lsp.buf.format()
-				["start"] = { 1, 0 },
-				["end"] = { last_line, last_column },
+	"nvimtools/none-ls.nvim",
+	dependencies = { "nvimtools/none-ls-extras.nvim" },
+	requires = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.biome.with({
+					prefer_local = "node_modules/.bin",
+					condition = is_biome_supported,
+				}),
+				null_ls.builtins.formatting.prettier.with({
+					prefer_local = "node_modules/.bin",
+					condition = is_prettier_supported,
+				}),
+				require("none-ls.diagnostics.eslint").with({
+					prefer_local = "node_modules/.bin",
+					condition = is_eslint_supported,
+				}),
+				require("none-ls.formatting.eslint").with({
+					prefer_local = "node_modules/.bin",
+					condition = is_eslint_supported,
+				}),
 			},
+			-- Configures autoformatting on save
+			-- on_attach = function(_, bufnr)
+			-- 	vim.api.nvim_create_autocmd("BufWritePre", {
+			-- 		group = augroup,
+			-- 		buffer = bufnr,
+			-- 		callback = function()
+			-- 			vim.lsp.buf.format()
+			-- 		end,
+			-- 	})
+			-- end,
 		})
-    end, { desc = "Format file or selection", silent = true })
-  end,
+
+		vim.keymap.set({ "n", "v" }, "ff", function()
+			local last_line = vim.fn.line("$")
+			local last_column = vim.fn.col({ last_line, "$" }) - 1
+			vim.lsp.buf.format({
+				range = {
+					-- Format from first to the last line of the the buffer for better performance
+					-- On visual mode it is automatically set to the visual selection
+					-- For some reason lines are 1 indexed and columns 0 🧐
+					-- See: https://neovim.io/doc/user/lsp.html#vim.lsp.buf.format()
+					["start"] = { 1, 0 },
+					["end"] = { last_line, last_column },
+				},
+			})
+		end, { desc = "Format file or selection", silent = true })
+	end,
 }
