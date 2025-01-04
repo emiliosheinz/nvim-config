92a5712eb2e251cbddbdb7de82cca63ce1fe36e4
Emilio Heinzmann <emiliosheinz@gmail.com>
2025-01-03 22:10:05 -0300

    refactor: change copilot accept shortcut

--- lua/emiliosheinz/plugins/github-copilot.lua
+++ lua/emiliosheinz/plugins/github-copilot.lua
@@ -5,7 +5,7 @@ return {
       vim.g.copilot_no_tab_map = true
     end,
     config = function()
-      vim.keymap.set("i", "<C-a>", 'copilot#Accept("\\<CR>")', {
+      vim.keymap.set("i", "<C-y>", 'copilot#Accept("\\<CR>")', {
         expr = true,
         replace_keycodes = false,
       })
