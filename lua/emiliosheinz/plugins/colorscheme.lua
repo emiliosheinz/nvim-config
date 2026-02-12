8628eab3855ede6ae6eb18da0a0331aebe19f7e2
Emilio Heinzmann <emiliosheinz@gmail.com>
2026-02-12 20:37:27 -0300

    refactor: drop color overrides

--- lua/emiliosheinz/plugins/colorscheme.lua
+++ lua/emiliosheinz/plugins/colorscheme.lua
@@ -17,11 +17,6 @@ return {
             TelescopeSelection = { bg = colors.surface0 },
           }
         end,
-        color_overrides = {
-          mocha = {
-            base = "#181825"
-          }
-        }
       })
       vim.cmd([[colorscheme catppuccin]])
     end,
