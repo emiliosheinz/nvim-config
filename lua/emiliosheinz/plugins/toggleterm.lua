79d8fe4d7dd59b86c9ebbc1e75490ff6bbf20c12
Emilio Heinzmann <emiliosheinz@gmail.com>
2026-02-15 09:16:00 -0300

    fix: toggle term size

--- lua/emiliosheinz/plugins/toggleterm.lua
+++ lua/emiliosheinz/plugins/toggleterm.lua
@@ -2,19 +2,17 @@ return {
   'akinsho/toggleterm.nvim',
   version = "*",
   config = function()
-    local screen_w = vim.opt.columns:get()
-    local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-    local window_h = screen_h * 0.96
-    local window_w_int = math.floor(screen_w)
-    local window_h_int = math.floor(window_h)
-
     require("toggleterm").setup({
       direction = 'float',
       float_opts = {
-        border =  'curved',
-        width = window_w_int,
-        height = window_h_int,
-        row = 0,
+        border = 'curved',
+        width = function()
+          return vim.o.columns
+        end,
+        height = function()
+          -- Subtract cmdheight and 2 additional line to prevent border cutoff
+          return vim.o.lines - vim.o.cmdheight - 2
+        end,
       },
     })
 
